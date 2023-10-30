64b6647e45d11b9b7089608d9d8131a43689adf5
Bishwas Bhandari <42182303+Bishwas-py@users.noreply.github.com>
2023-10-30 21:42:40 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,10 @@ setup(
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
         'Programming Language :: Python :: 3.11',
         'Framework :: Django',
         'Framework :: Django :: 4.0',
